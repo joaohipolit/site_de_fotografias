2933eee247f023b5e598bb0561c0fa21cbe8cf2e
joao hipolito <joaopedrohipolito215@gmail.com>
2024-12-01 01:31:02 +0000

    atualizei o html.html o css.css e o java.js

--- java.js
+++ java.js
@@ -1,16 +1,17 @@
 // Função para abrir o lightbox com a imagem selecionada
 /*java.js*/
 function openLightbox(src) {
-    const lightbox = document.getElementById("lightbox");
-    const lightboxImage = document.getElementById("lightbox-image");
-  
-    lightboxImage.src = src;
-    lightbox.style.display = "flex";
-  }
-  
-  // Função para fechar o lightbox ao clicar fora da imagem
-  function closeLightbox() {
-    const lightbox = document.getElementById("lightbox");
-    lightbox.style.display = "none";
-  }
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImage = document.getElementById("lightbox-image");
+
+  lightboxImage.src = src;
+  lightbox.style.display = "flex";
+}
+
+// Função para fechar o lightbox ao clicar fora da imagem
+function closeLightbox() {
+  const lightbox = document.getElementById("lightbox");
+  lightbox.style.display = "none";
+}
+
   
